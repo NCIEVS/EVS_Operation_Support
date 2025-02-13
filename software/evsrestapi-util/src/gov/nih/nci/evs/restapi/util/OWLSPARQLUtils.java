ee1d8a6afaaa7f822503ad90bcc2b4ea07a2ecb9
kimongl <kim.ong@nih.gov>
2025-02-13 15:43:19 -0500

    Updated OWLSPARQLUtils.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/OWLSPARQLUtils.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/OWLSPARQLUtils.java
@@ -5930,7 +5930,6 @@ bnode_07130346_a093_4c67_ad70_efd4d5bc5796_242618|Thorax|C12799|Maps_To|P375|Tho
         return getSuperclass(named_graph);
 	}
 
-
 	public String construct_get_superclass(String named_graph) {
         String prefixes = getPrefixes();
         StringBuffer buf = new StringBuffer();
@@ -5939,6 +5938,7 @@ bnode_07130346_a093_4c67_ad70_efd4d5bc5796_242618|Thorax|C12799|Maps_To|P375|Tho
         buf.append("{").append("\n");
         buf.append("graph <" + named_graph + ">").append("\n");
         buf.append("{").append("\n");
+        buf.append("{").append("\n");
         buf.append("?y a owl:Class .").append("\n");
         buf.append("?y :NHC0 ?y_code .").append("\n");
         buf.append("?y rdfs:label ?y_label .").append("\n");
@@ -5946,6 +5946,17 @@ bnode_07130346_a093_4c67_ad70_efd4d5bc5796_242618|Thorax|C12799|Maps_To|P375|Tho
         buf.append("?x :NHC0 ?x_code .").append("\n");
         buf.append("?x rdfs:label ?x_label .").append("\n");
         buf.append("?x (rdfs:subClassOf|(owl:equivalentClass/owl:intersectionOf/rdf:rest*/rdf:first)) ?y .").append("\n");
+        buf.append("}").append("\n");
+        buf.append("UNION").append("\n");
+        buf.append("{").append("\n");
+        buf.append("?y a owl:Class .").append("\n");
+        buf.append("?y :NHC0 ?y_code .").append("\n");
+        buf.append("?y rdfs:label ?y_label .").append("\n");
+        buf.append("?x a owl:Class .").append("\n");
+        buf.append("?x :NHC0 ?x_code .").append("\n");
+        buf.append("?x rdfs:label ?x_label .").append("\n");
+        buf.append("?x (rdfs:subClassOf/owl:intersectionOf/rdf:rest*/rdf:first) ?y .").append("\n");
+        buf.append("}").append("\n");
         buf.append("}").append("\n");
         buf.append("}").append("\n");
         return buf.toString();
@@ -5959,7 +5970,6 @@ bnode_07130346_a093_4c67_ad70_efd4d5bc5796_242618|Thorax|C12799|Maps_To|P375|Tho
         if (v.size() == 0) return v;
         return new SortUtils().quickSort(v);
 	}
-
 
 	public String construct_get_hierarchical_relationships(String named_graph) {
 		String prefixes = getPrefixes();
