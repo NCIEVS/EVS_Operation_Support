de69fc548529ddaf1999cb75525f8c4a3d6625bc
kimongl <kim.ong@nih.gov>
2025-06-11 09:33:05 -0400

    Updated IndexUtils.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/IndexUtils.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/IndexUtils.java
@@ -56,16 +56,56 @@ import org.apache.commons.text.similarity.*;
  *
  */
 public class IndexUtils {
+
+	public static String[] specialChars = new String[] {"{", "}", "(", ")", "'", ":", ";", ".", "," ,"\""};
+	public static HashSet specialCharsHashSet = toHashSet(specialChars);
+
 	String serviceUrl = null;
 	String namedGraph = null;
 	String username = null;
 	String password = null;
 	SPARQLSearchUtils searchUtils = null;
 
+    public static HashSet STOP_WORDS = null;
+    public static HashSet KEYWORDS = null;
+    public static HashMap signatureMap = null;
+    public static HashMap id2LabelMap = null;
+
+    static {
+		STOP_WORDS = LexicalMatching.STOP_WORDS;
+		KEYWORDS = LexicalMatching.KEYWORDS;
+		signatureMap = LexicalMatching.signatureMap;
+		id2LabelMap = LexicalMatching.id2LabelMap;
+		System.out.println("KEYWORDS: " + KEYWORDS.size());
+		System.out.println("STOP_WORDS: " + STOP_WORDS.size());
+	}
+
+    public IndexUtils() {
+
+    }
+
     public IndexUtils(String serviceUrl, String namedGraph, String username, String password) {
 		this.serviceUrl = serviceUrl;
 		this.namedGraph = namedGraph;
         searchUtils = new SPARQLSearchUtils(serviceUrl, namedGraph, username, password);
+	}
+
+	public static HashSet toHashSet(Vector a) {
+		HashSet hset = new HashSet();
+		for (int i=0; i<a.size(); i++) {
+			String t = (String) a.elementAt(i);
+			hset.add(t);
+		}
+		return hset;
+	}
+
+	public static HashSet toHashSet(String[] a) {
+		HashSet hset = new HashSet();
+		for (int i=0; i<a.length; i++) {
+			String t = a[i];
+			hset.add(t);
+		}
+		return hset;
 	}
 
     public Vector toSentences(String narrative) {
@@ -78,6 +118,16 @@ public class IndexUtils {
 		return w;
 	}
 
+	public Vector addAll(Vector v, Vector u) {
+		for (int i=0; i<u.size(); i++) {
+			String t = (String) u.elementAt(i);
+			if (!v.contains(t)) {
+				v.add(t);
+			}
+		}
+		return v;
+	}
+
 	public Vector indexNarrative(String narrative) {
 		Vector w = toSentences(narrative);
 		Vector v = new Vector();
@@ -86,27 +136,43 @@ public class IndexUtils {
 			Vector segments = toSegments(sentence);
 			for (int j=0; j<segments.size(); j++) {
 				String segment = (String) segments.elementAt(j);
-				Vector u = searchUtils.indexTerm(segment);
+				Vector u = indexTerm(segment);
 				if (u != null && u.size() >0) {
-					v.addAll(u);
+					addAll(v, u);
 				}
 			}
 		}
 		return v;
 	}
 
+    public static boolean isKeyword(String word) {
+		return KEYWORDS.contains(word);
+	}
+
+    public static boolean isFiller(String word) {
+		return STOP_WORDS.contains(word);
+	}
+
+	public Vector term2Keywords(String sentence) {
+		char c = sentence.charAt(sentence.length()-1);
+		while (!Character.isLetterOrDigit(c)) {
+			sentence = sentence.substring(0, sentence.length()-1);
+			c = sentence.charAt(sentence.length()-1);
+		}
+		return LexicalMatching.tokenize(sentence);
+	}
+
 	public Vector toSegments(String sentence) {
-		System.out.println(sentence);
 		Vector v = new Vector();
-		Vector w = SPARQLSearchUtils.term2Keywords(sentence);
+		Vector w = term2Keywords(sentence);
 		StringBuffer buf = new StringBuffer();
 		for (int i=0; i<w.size(); i++) {
 			String word = (String) w.elementAt(i);
-			boolean isKeyword = searchUtils.isKeyword(word);
-            boolean isFiller = searchUtils.isFiller(word);
+			boolean isKeyword = isKeyword(word);
+            boolean isFiller = isFiller(word);
             if (isKeyword && !isFiller) {
 				buf.append(word).append(" ");
-			} else if( !isKeyword) {
+			} else if(!isKeyword) {
 				String t = buf.toString();
 				t = t.trim();
 				v.add(t);
@@ -122,19 +188,147 @@ public class IndexUtils {
 
 	}
 
-    public static void main(String[] args) {
-		long ms = System.currentTimeMillis();
-		String restURL = args[0];
-		String namedGraph = args[1];
-		String username = args[2];
-		String password = args[3];
-	    IndexUtils indexUtils = new IndexUtils(restURL, namedGraph, username, password);
+	public String getLabel(String code) {
+		return (String) id2LabelMap.get(code);
+	}
 
-		String narrative = "The most commonly reported side effects, which typically lasted several days, were pain at the injection site, tiredness, headache, muscle pain, chills, joint pain, swollen lymph nodes in the same arm as the injection, nausea and vomiting, and fever. Of note, more people experienced these side effects after the second dose than after the first dose, so it is important for vaccination providers and recipients to expect that there may be some side effects after either dose, but even more so after the second dose.";
-		Vector w = indexUtils.indexNarrative(narrative);
-		Utils.dumpVector(narrative, w);
+	public Vector getCodeBySignature(String signature) {
+		if (signatureMap.containsKey(signature)) {
+			return (Vector) signatureMap.get(signature);
+		}
+		return null;
+	}
 
-    }
+	public static Vector toKeywords(String term) {
+		HashSet hset = new HashSet();
+		term = term.trim();
+		term = term.replace("\"", "");
+		term = term.trim();
+		if (term.length() > 0) {
+			term = term.toLowerCase();
+			term = term.replace("/", " ");
+			String[] tokens = term.split(" ");
+			for (int j=0; j<tokens.length; j++) {
+				String token = tokens[j];
+				if (token.length() > 1) {
+					char firstChar = token.charAt(0);
+					char lastChar = token.charAt(token.length()-1);
+					if (specialCharsHashSet.contains("" + firstChar) &&
+						specialCharsHashSet.contains("" + lastChar)) {
+						token = token.substring(1, token.length()-1);
+					} else if (specialCharsHashSet.contains("" + lastChar)) {
+						token = token.substring(0, token.length()-1);
+					} else if (specialCharsHashSet.contains("" + firstChar)) {
+						token = token.substring(1, token.length());
+					}
+					if (token.length() > 0) {
+						if (!hset.contains(token)) {
+							hset.add(token);
+						}
+					}
+				} else if (token.length() == 1) {
+					if (!hset.contains(token)) {
+						hset.add(token);
+					}
+				}
+			}
+		}
+		return new SortUtils().quickSort(Utils.hashSet2Vector(hset));
+	}
+
+    public String getSignature(String term) {
+		return LexicalMatching.getSignature(term);
+	}
+
+	public String words2Term(Vector words) {
+		StringBuffer buf = new StringBuffer();
+		for (int i=0; i<words.size(); i++) {
+			String word = (String) words.elementAt(i);
+			buf.append(word);
+			if (i < words.size()-1) {
+				buf.append(" ");
+			}
+		}
+		return buf.toString();
+	}
+
+	public Vector matchBySignature(Vector words) {
+		String signature = getSignature(words2Term(words));
+		return getCodeBySignature(signature);
+	}
+
+	public Vector matchBySignature(String term) {
+		String signature = getSignature(term);
+		return getCodeBySignature(signature);
+	}
+
+	public boolean cooccurrent(String word1, String word2) {
+		Vector words = new Vector();
+		words.add(word1);
+		words.add(word2);
+		Vector w = matchBySignature(words);
+		if (w != null && w.size() > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public Vector indexTerm(String term) {
+		Vector w = matchBySignature(term);
+		if (w != null && w.size() > 0) return w;
+
+		w = new Vector();
+        Vector words = term2Keywords(term);
+        Vector v0 = new Vector();
+        if (words == null || words.size() == 0) return w;
+        String vbt = "";
+        Vector v = null;
+        String prevword = "";
+        String nextword = "";
+        while (words.size() > 0) {
+			nextword = (String) words.remove(0);
+			boolean cooccur = true;
+			if (prevword.length() > 0) {
+				cooccur = cooccurrent(prevword, nextword);
+			}
+			if (cooccur) {
+				vbt = vbt + " " + nextword;
+			} else {
+				if (v0 != null && v0.size() > 0) {
+					w.addAll(v0);
+					v0 = null;
+			    }
+				vbt = nextword;
+			}
+			v = matchBySignature(vbt);
+			if (v != null && v.size() > 0) {
+				v0 = v;
+			}
+			prevword = nextword;
+		}
+		if (v0 != null && v0.size() > 0) {
+			w.addAll(v0);
+			v0 = null;
+		}
+		Vector w1 = new Vector();
+		if (w == null || w.size() == 0) return w1;
+		for (int i=0; i<w.size(); i++) {
+			String code = (String) w.elementAt(i);
+			String label = getLabel(code);
+			w1.add(label + "|" + code);
+		}
+		return new SortUtils().quickSort(w1);
+	}
+
+    public Vector appendLabels(Vector codes) {
+        Vector w = new Vector();
+        for (int i=0; i<codes.size(); i++) {
+			String code = (String) codes.elementAt(i);
+			w.add(getLabel(code) + " (" + code + ")");
+		}
+		w = new SortUtils().quickSort(w);
+		return w;
+	}
 
 }
 
