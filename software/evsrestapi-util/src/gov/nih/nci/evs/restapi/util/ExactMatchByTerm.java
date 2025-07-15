5d212890a5d4d1f3b65fc86c58874c5ba3e0f539
kimongl <kim.ong@nih.gov>
2025-07-15 07:34:12 -0400

    Modified ExactMatchByTerm.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ExactMatchByTerm.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/ExactMatchByTerm.java
@@ -196,7 +196,6 @@ public class ExactMatchByTerm {
 				}
 			}
 		}
-        //Utils.dumpHashMap("source_code.map", hmap);
         return hmap;
 	}
 
@@ -228,13 +227,6 @@ public class ExactMatchByTerm {
 
     public static HashSet createRetiredConceptSet() {
 		HashSet hset = new HashSet();
-		/*
-		File file = new File(CONCEPT_STATUS_FILE);
-		if (!file.exists()) {
-			System.out.println("WARNING: " + CONCEPT_STATUS_FILE + " does not exist.");
-			extractProperties("P310");
-		}
-		*/
 		extractProperties("P310");
 		Vector v = Utils.readFile(CONCEPT_STATUS_FILE);
 		for (int i=0; i<v.size(); i++) {
@@ -264,7 +256,7 @@ public class ExactMatchByTerm {
 
     public static HashMap createSourcePTMap(String source) {
 		HashMap hmap = new HashMap();
-		Vector v = readFile(AXIOM_FILE);
+		Vector v = Utils.readFile(AXIOM_FILE);
 		Vector w = new Vector();
 		for (int i=0; i<v.size(); i++) {
 			String line = (String) v.elementAt(i);
@@ -279,7 +271,7 @@ public class ExactMatchByTerm {
     //Recombinant Amphiregulin|C1000|P90|AR|P383$AB|P384$NCI
     public static HashMap createSourceSYMap(String source) {
 		HashMap hmap = new HashMap();
-		Vector v = readFile(AXIOM_FILE);
+		Vector v = Utils.readFile(AXIOM_FILE);
 		Vector w = new Vector();
 		for (int i=0; i<v.size(); i++) {
 			String line = (String) v.elementAt(i);
@@ -307,7 +299,7 @@ public class ExactMatchByTerm {
 
     public static HashMap createSourceCode2NCItCodesMap(String source) {
 		HashMap sourceCode2NCItCodesMap = new HashMap();
-		Vector v = readFile(AXIOM_FILE);
+		Vector v = Utils.readFile(AXIOM_FILE);
 		String target = "P384$" + source;
 		Vector w = new Vector();
 		for (int i=0; i<v.size(); i++) {
@@ -341,481 +333,6 @@ public class ExactMatchByTerm {
 		return sourceCode2NCItCodesMap;
 	}
 
-    public static Vector parseData(String line, char delimiter) {
-		if(line == null) return null;
-		Vector w = new Vector();
-		StringBuffer buf = new StringBuffer();
-		for (int i=0; i<line.length(); i++) {
-			char c = line.charAt(i);
-			if (c == delimiter) {
-				w.add(buf.toString());
-				buf = new StringBuffer();
-			} else {
-				buf.append(c);
-			}
-		}
-		w.add(buf.toString());
-		return w;
-	}
-
-    public static void dumpHashMap(String label, HashMap hmap) {
-		System.out.println("\n" + label + ":");
-		if (hmap == null) {
-			System.out.println("\tNone");
-			return;
-		}
-		Vector key_vec = new Vector();
-		Iterator it = hmap.keySet().iterator();
-		while (it.hasNext()) {
-			String key = (String) it.next();
-			key_vec.add(key);
-		}
-		key_vec = new SortUtils().quickSort(key_vec);
-		for (int i=0; i<key_vec.size(); i++) {
-			String key = (String) key_vec.elementAt(i);
-			String value = (String) (String) hmap.get(key);
-			System.out.println(key + " --> " + value);
-		}
-		System.out.println("\n");
-	}
-
-    public static void dumpMultiValuedHashMap(String label, HashMap hmap) {
-   	    System.out.println("\n" + label + ":");
-		if (hmap == null) {
-			System.out.println("\tNone");
-			return;
-		}
-		Vector key_vec = new Vector();
-		Iterator it = hmap.keySet().iterator();
-		while (it.hasNext()) {
-			String nv = (String) it.next();
-			key_vec.add(nv);
-		}
-		key_vec = new SortUtils().quickSort(key_vec);
-		for (int k=0; k<key_vec.size(); k++) {
-			String nv = (String) key_vec.elementAt(k);
-			System.out.println("\n");
-			Vector v = (Vector) hmap.get(nv);
-			for (int i=0; i<v.size(); i++) {
-				String q = (String) v.elementAt(i);
-				System.out.println(nv + " --> " + q);
-			}
-		}
-		System.out.println("\n");
-	}
-
-    public static void dumpVector(String label, Vector v) {
-		System.out.println("\n" + label + ":");
-		if (v == null) return;
-		if (v.size() == 0) {
-			System.out.println("\tNone");
-			return;
-		}
-        for (int i=0; i<v.size(); i++) {
-			String t = (String) v.elementAt(i);
-			int j = i+1;
-			System.out.println("\t(" + j + ") " + t);
-		}
-		System.out.println("\n");
-	}
-
-    public static void dumpVector(String label, Vector v, boolean display_label, boolean display_index) {
-		if (display_label) {
-			System.out.println("\n" + label + ":");
-		}
-		if (v == null || v.size() == 0) {
-			System.out.println("\tNone");
-			return;
-		}
-        for (int i=0; i<v.size(); i++) {
-			String t = (String) v.elementAt(i);
-			int j = i+1;
-			if (display_index) {
-				System.out.println("\t(" + j + ") " + t);
-			} else {
-				System.out.println("\t" + t);
-			}
-		}
-		System.out.println("\n");
-	}
-
-    public static void dumpArrayList(String label, ArrayList list) {
-		System.out.println("\n" + label + ":");
-		if (list == null || list.size() == 0) {
-			System.out.println("\tNone");
-			return;
-		}
-        for (int i=0; i<list.size(); i++) {
-			String t = (String) list.get(i);
-			int j = i+1;
-			System.out.println("\t(" + j + ") " + t);
-		}
-		System.out.println("\n");
-	}
-
-    public static void dumpList(String label, List list) {
-		System.out.println("\n" + label + ":");
-		if (list == null || list.size() == 0) {
-			System.out.println("\tNone");
-			return;
-		}
-        for (int i=0; i<list.size(); i++) {
-			String t = (String) list.get(i);
-			int j = i+1;
-			System.out.println("\t(" + j + ") " + t);
-		}
-		System.out.println("\n");
-	}
-
-
-	 public static String replaceFilename(String filename) {
-	    return filename.replaceAll("[^a-zA-Z0-9\\.\\-]", "_");
-	 }
-
-	 public static void saveToFile(String outputfile, String t) {
-		 Vector v = new Vector();
-		 v.add(t);
-		 saveToFile(outputfile, v);
-	 }
-
-	public static void saveToFile(PrintWriter pw, Vector v) {
-		if (v != null && v.size() > 0) {
-			for (int i=0; i<v.size(); i++) {
-				String t = (String) v.elementAt(i);
-				pw.println(t);
-			}
-		}
-	}
-
-	public static Vector readFile(String datafile) {
-		Vector v = new Vector();
-        try {
-			File file = new File(datafile);
-			FileInputStream fis = new FileInputStream(file);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			BufferedReader br = null;
-			try {
-				br = new BufferedReader(new InputStreamReader(bis));
-			} catch (Exception ex) {
-				return null;
-			}
-
-            while (true) {
-                String line = br.readLine();
-				if (line == null) {
-					break;
-				}
-				v.add(line);
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return v;
-	}
-
-	public static HashSet vector2HashSet(Vector v) {
-		if (v == null) return null;
-		HashSet hset = new HashSet();
-		for (int i=0; i<v.size(); i++) {
-			String t = (String) v.elementAt(i);
-			hset.add(t);
-		}
-		return hset;
-	}
-
-	public static Vector hashSet2Vector(HashSet hset) {
-		if (hset == null) return null;
-		Vector v = new Vector();
-		Iterator it = hset.iterator();
-		while (it.hasNext()) {
-			String t = (String) it.next();
-			v.add(t);
-		}
-		v = new SortUtils().quickSort(v);
-		return v;
-	}
-
-    public static String changeFileExtension(String filename, String ext) {
-		int n = filename.lastIndexOf(".");
-		if (n != -1) {
-			return filename.substring(0, n) + "." + ext;
-		}
-		return filename;
-	}
-
-	public static HashMap getInverseHashMap(HashMap hmap) {
-		HashMap inv_hmap = new HashMap();
-		Iterator it = hmap.keySet().iterator();
-		while (it.hasNext()) {
-			String key = (String) it.next();
-			String value = (String) hmap.get(key);
-			Vector v = new Vector();
-			if (inv_hmap.containsKey(value)) {
-				v = (Vector) inv_hmap.get(value);
-			}
-			v.add(key);
-			inv_hmap.put(value, v);
-		}
-		return inv_hmap;
-	}
-
-    public static Vector listFiles(String directory) {
-		Vector w = new Vector();
-		Collection<File> c = listFileTree(new File(directory));
-		int k = 0;
-		Iterator it = c.iterator();
-		while (it.hasNext()) {
-			File t = (File) it.next();
-			k++;
-			w.add(t.getName());
-		}
-		w = new SortUtils().quickSort(w);
-		return w;
-	}
-
-
-	public static Collection<File> listFileTree(File dir) {
-		Set<File> fileTree = new HashSet<File>();
-		if(dir==null||dir.listFiles()==null){
-			return fileTree;
-		}
-		for (File entry : dir.listFiles()) {
-			if (entry.isFile()) fileTree.add(entry);
-			else fileTree.addAll(listFileTree(entry));
-		}
-		return fileTree;
-	}
-
-    public static boolean checkIfFileExists(String filename) {
-		String currentDir = System.getProperty("user.dir");
-		File f = new File(currentDir + "\\" + filename);
-		if(f.exists() && !f.isDirectory()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-    public static boolean deleteFile(String filename) {
-        File file = new File(filename);
-        if(file.delete())
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-
-
-	public static void copyDirectory(String sourceDirectoryLocation, String destinationDirectoryLocation)
-	  throws IOException {
-	    Files.walk(java.nio.file.Paths.get(sourceDirectoryLocation))
-	      .forEach(source -> {
-		  java.nio.file.Path destination = java.nio.file.Paths.get(destinationDirectoryLocation, source.toString()
-		    .substring(sourceDirectoryLocation.length()));
-		  try {
-		      Files.copy(source, destination);
-		  } catch (IOException e) {
-		      e.printStackTrace();
-		  }
-	      });
-	}
-
-
-	public static String encode(String toEncode) {
-		try {
-			return java.net.URLEncoder.encode(toEncode.trim(), "UTF-8");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return null;
-	}
-
-    public static String generateHyperlink(String name, String propertyName) {
-		String propertyName_0 = propertyName;
-		if (propertyName.compareTo("Relationships") == 0) {
-			propertyName = "list_relationships";
-		}
-		//propertyName = propertyName.replace(" ", "+");
-		String hyperlink = "https://aapmbdsc.azurewebsites.net/?KeyElementName=" + name + "&PropertyName=" + propertyName;
-		//hyperlink = hyperlink.replace("�", "&ndash;");
-		//hyperlink = encode(hyperlink);
-		hyperlink = "<a href=\"" + hyperlink + "\">" + propertyName_0 + "</a>";
-		return hyperlink;
-	}
-
-
-	public static boolean isPureAscii(String v) {
-		return Charset.forName("US-ASCII").newEncoder().canEncode(v);
-	}
-
-	public String xmlEscapeText(String t) {
-		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < t.length(); i++){
-			char c = t.charAt(i);
-			switch(c){
-				case '<': sb.append("&lt;"); break;
-				case '>': sb.append("&gt;"); break;
-				case '\"': sb.append("&quot;"); break;
-				case '&': sb.append("&amp;"); break;
-				case '\'': sb.append("&apos;"); break;
-				default:
-					if (c>0x7e) {
-						sb.append("&#"+((int)c)+";");
-					} else {
-						sb.append(c);
-					}
-			}
-		}
-		return sb.toString();
-	}
-
-	public static Vector loadFileLineByLine(String filename) {
-		Vector w = new Vector();
-		StringBuffer buf = new StringBuffer();
-		try {
-			InputStream input = new FileInputStream(filename);
-			int data = input.read();
-			while(data != -1) {
-				char c = (char) data;
-				if (c == '\n') {
-					String s = buf.toString();
-					s = s.trim();
-					w.add(s);
-					buf = new StringBuffer();
-				} else {
-					buf.append("" + c);
-				}
-				data = input.read();
-			}
-			String s = buf.toString();
-			s = s.trim();
-			w.add(s);
-			input.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return w;
-	}
-
-    public static void saveToFile(String outputfile, Vector v) {
-        try {
-            FileOutputStream output = new FileOutputStream(outputfile);
-            for (int i=0; i<v.size(); i++) {
-				String data = (String) v.elementAt(i);
-				if (i < v.size()) {
-					data = data + "\n";
-				}
-				byte[] array = data.getBytes();
-				output.write(array);
-			}
-            output.close();
-        } catch(Exception e) {
-            e.getStackTrace();
-        }
-    }
-
-	public static String extractTagValue(String line, String tag) {
-		String t0 = line;
-		String t = t0.trim();
-		int n = t.indexOf(tag);
-		if (n == -1) return null;
-		t = t.substring(n+tag.length(), t.length());
-		n = t.indexOf("<");
-		if (n == -1) return null;
-		t = t.substring(1, n);
-		t = t.trim();
-		return t;
-	}
-
-	public static HashSet extractColumnData(String filename, int col, char delim) {
-		Vector v = readFile(filename);
-		HashSet hset = new HashSet();
-		for (int i=0; i<v.size(); i++) {
-			String line = (String) v.elementAt(i);
-			Vector u = StringUtils.parseData(line, delim);
-			String value = (String) u.elementAt(col);
-			if (!hset.contains(value)) {
-				hset.add(value);
-			}
-		}
-		return hset;
-	}
-//C192234	Operational Ontology for Radiation Oncology Prostate Cancer Terminology	C104495	Other Race
-
-    public static void exploreContent(String filename, char delim) {
-        Vector v = readFile(filename);
-        String firstLine = (String) v.elementAt(0);
-        Vector u = StringUtils.parseData(firstLine, delim);
-        Utils.dumpVector(firstLine, u);
-	}
-
-	public static void mergeFiles(String sourcefile, int keyCol_1, String targetfile, int keyCol_2, String heading) {
-		long ms = System.currentTimeMillis();
-		//String codedTerms = "CodedTerms.txt";
-
-        Vector v1 = readFile(sourcefile);
-        HashMap hmap = new HashMap();
-        for (int i=1; i<v1.size(); i++) {
-			String line = (String) v1.elementAt(i);
-			Vector u = StringUtils.parseData(line, '\t');
-			String t =(String) u.elementAt(keyCol_1);
-			hmap.put(t, line);
-		}
-        Vector v2 = readFile(targetfile);
-        Vector w = new Vector();
-        w.add(heading);//"Term\tNCIt PT\tNCIt Code");
-        int knt1 = 0;
-        int knt2 = 0;
-        for (int i=0; i<v2.size(); i++) {
-			String line = (String) v2.elementAt(i);
-			Vector u = StringUtils.parseData(line, '\t');
-			String t = (String) u.elementAt(keyCol_2);
-			if (hmap.containsKey(t)) {
-				w.add(t + "\t" + (String) hmap.get(t));
-				knt1++;
-			} else {
-				w.add(t + "\tNot found\tNot found");
-				knt2++;
-			}
-		}
-		Utils.saveToFile("mod_" + targetfile, w);
-		System.out.println("No match: " + knt2);
-		System.out.println("Total run time (ms): " + (System.currentTimeMillis() - ms));
-	}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-    public Vector extractColumnValues(String filename, int columnIndex, char delim) {
-		Vector v = readFile(filename);
-		Vector w = new Vector();
-		for (int i=0; i<v.size(); i++) {
-			String line = (String) v.elementAt(i);
-			Vector u = StringUtils.parseData(line, delim);
-			String value = (String) u.elementAt(columnIndex);
-			w.add(value);
-		}
-		return w;
-	}
-
-	public static List<String> findFilesInDirectory(String directory) {
-		return findFilesInDirectory(new File(directory));
-	}
-
-	public static List<String> findFilesInDirectory(File dir) {
-		List<String> list = new ArrayList<String>();
-		for (File file : dir.listFiles()) {
-			if (file.isDirectory()) {
-				List<String> list2 = findFilesInDirectory(file);
-				list.addAll(list2);
-			} else {
-				list.add(file.getAbsolutePath());
-			}
-		}
-		return list;
-	}
-
 //Case sensitive match
     public static HashMap createTerm2CodesMap() {
 		return createTerm2CodesMap(null);
@@ -826,7 +343,7 @@ public class ExactMatchByTerm {
 		if (filename == null) {
 			filename = AXIOM_FILE;
 		}
-		Vector v = readFile(filename);
+		Vector v = Utils.readFile(filename);
         for (int i=0; i<v.size(); i++) {
 			String line = (String) v.elementAt(i);
 			Vector u = StringUtils.parseData(line, '|');
@@ -856,7 +373,7 @@ public class ExactMatchByTerm {
     public static HashMap createCode2FULLSYNMap() {
 		String filename = AXIOM_FILE;
 		HashMap hmap = new HashMap();
-		Vector v = readFile(filename);
+		Vector v = Utils.readFile(filename);
         for (int i=0; i<v.size(); i++) {
 			String line = (String) v.elementAt(i);
 			Vector u = StringUtils.parseData(line, '|');
