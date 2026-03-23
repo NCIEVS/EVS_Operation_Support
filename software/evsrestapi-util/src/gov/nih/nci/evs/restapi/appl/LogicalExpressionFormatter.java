16250712dd9fbc4a8d35cf4f1f23f1c1948d1f20
kimongl <kim.ong@nih.gov>
2026-03-23 19:24:31 -0400

    Updated LogicalExpressionFormatter.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/appl/LogicalExpressionFormatter.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/appl/LogicalExpressionFormatter.java
@@ -132,76 +132,80 @@ public class LogicalExpressionFormatter {
 		}
 		map.put("ROLE", w);
 		Vector roleunions = (Vector) hmap.get("ROLE UNION");
-		w = new Vector();
-		Vector ids = new Vector();
-		for (int i=0; i<roleunions.size(); i++) {
-			String line = (String) roleunions.elementAt(i);
-			Vector u = StringUtils.parseData(line, '|');
-			if (u.size() > 1) {
-				String id = (String) u.elementAt(2);
-				if (!ids.contains(id)) {
-					ids.add(id);
-				}
-			}
-		}
-        for (int i=0; i<ids.size(); i++) {
-			String id = (String) ids.elementAt(i);
-			List<Restriction> list = new ArrayList();
-			for (int j=0; j<roleunions.size(); j++) {
-				String line = (String) roleunions.elementAt(j);
+		if (roleunions != null && roleunions.size() > 0) {
+			w = new Vector();
+			Vector ids = new Vector();
+			for (int i=0; i<roleunions.size(); i++) {
+				String line = (String) roleunions.elementAt(i);
 				Vector u = StringUtils.parseData(line, '|');
-				if (u.size() > 2) {
-					String line_id = (String) u.elementAt(2);
-					if (line_id.compareTo(id) == 0) {
-						Restriction r = line2Restriction(line);
-						list.add(r);
+				if (u.size() > 1) {
+					String id = (String) u.elementAt(2);
+					if (!ids.contains(id)) {
+						ids.add(id);
 					}
 				}
 			}
-			RoleUnion ru = new RoleUnion(list);
-			w.add(ru);
+			for (int i=0; i<ids.size(); i++) {
+				String id = (String) ids.elementAt(i);
+				List<Restriction> list = new ArrayList();
+				for (int j=0; j<roleunions.size(); j++) {
+					String line = (String) roleunions.elementAt(j);
+					Vector u = StringUtils.parseData(line, '|');
+					if (u.size() > 2) {
+						String line_id = (String) u.elementAt(2);
+						if (line_id.compareTo(id) == 0) {
+							Restriction r = line2Restriction(line);
+							list.add(r);
+						}
+					}
+				}
+				RoleUnion ru = new RoleUnion(list);
+				w.add(ru);
+			}
+			map.put("ROLE UNION", w);
 		}
-		map.put("ROLE UNION", w);
 		Vector rolegroups = (Vector) hmap.get("ROLE GROUP");
-		ids = new Vector();
-		for (int i=0; i<rolegroups.size(); i++) {
-			String line = (String) rolegroups.elementAt(i);
-			Vector u = StringUtils.parseData(line, '|');
-			if (u.size() > 2) {
-				String id = (String) u.elementAt(2);
-				if (!ids.contains(id)) {
-					ids.add(id);
-				}
-			}
-		}
-
-        w = new Vector();
-        List<Restriction> list = new ArrayList();
-        for (int i=0; i<ids.size(); i++) {
-			String id = (String) ids.elementAt(i);
-			List<RolePair> rp_list = new ArrayList();
-			for (int j=0; j<rolegroups.size(); j++) {
-				String line = (String) rolegroups.elementAt(j);
+		if (rolegroups != null && rolegroups.size() > 0) {
+			Vector ids = new Vector();
+			for (int i=0; i<rolegroups.size(); i++) {
+				String line = (String) rolegroups.elementAt(i);
 				Vector u = StringUtils.parseData(line, '|');
 				if (u.size() > 2) {
-					String line_id = (String) u.elementAt(2);
-					if (line_id.compareTo(id) == 0) {
-						Restriction r = line2Restriction(line);
-						list.add(r);
+					String id = (String) u.elementAt(2);
+					if (!ids.contains(id)) {
+						ids.add(id);
 					}
 				}
 			}
-			for (int k=0; k<list.size()/2; k++) {
-				Restriction r1 = (Restriction) list.get(k);
-				Restriction r2 = (Restriction) list.get(k+1);
-				RolePair rp = new RolePair(r1, r2);
-				rp_list.add(rp);
+
+			w = new Vector();
+			List<Restriction> list = new ArrayList();
+			for (int i=0; i<ids.size(); i++) {
+				String id = (String) ids.elementAt(i);
+				List<RolePair> rp_list = new ArrayList();
+				for (int j=0; j<rolegroups.size(); j++) {
+					String line = (String) rolegroups.elementAt(j);
+					Vector u = StringUtils.parseData(line, '|');
+					if (u.size() > 2) {
+						String line_id = (String) u.elementAt(2);
+						if (line_id.compareTo(id) == 0) {
+							Restriction r = line2Restriction(line);
+							list.add(r);
+						}
+					}
+				}
+				for (int k=0; k<list.size()/2; k++) {
+					Restriction r1 = (Restriction) list.get(k);
+					Restriction r2 = (Restriction) list.get(k+1);
+					RolePair rp = new RolePair(r1, r2);
+					rp_list.add(rp);
+				}
+				RoleGroup rg = new RoleGroup(rp_list);
+				//System.out.println(rg.toJson());
+				w.add(rg);
 			}
-			RoleGroup rg = new RoleGroup(rp_list);
-			//System.out.println(rg.toJson());
-			w.add(rg);
+			map.put("ROLE GROUP", w);
 		}
-		map.put("ROLE GROUP", w);
 		return map;
 	}
 
@@ -221,35 +225,39 @@ public class LogicalExpressionFormatter {
 				ranges.add(range);
 			}
 		}
-		for (int i=0; i<roleUnions.size(); i++) {
-			RoleUnion ru = (RoleUnion) roleUnions.elementAt(i);
-			List<Restriction> restrictions = ru.getRoles();
-			for (int j=0; j<restrictions.size(); j++) {
-				Restriction r = (Restriction) restrictions.get(j);
-				//System.out.println(r.toJson());
-				String label = (String) r.getRoleLabel();
-				String range = (String) roleName2RangeNameMap.get(label);
-				if (!ranges.contains(range)) {
-					ranges.add(range);
+		if (roleUnions != null && roleUnions.size() > 0) {
+			for (int i=0; i<roleUnions.size(); i++) {
+				RoleUnion ru = (RoleUnion) roleUnions.elementAt(i);
+				List<Restriction> restrictions = ru.getRoles();
+				for (int j=0; j<restrictions.size(); j++) {
+					Restriction r = (Restriction) restrictions.get(j);
+					//System.out.println(r.toJson());
+					String label = (String) r.getRoleLabel();
+					String range = (String) roleName2RangeNameMap.get(label);
+					if (!ranges.contains(range)) {
+						ranges.add(range);
+					}
 				}
 			}
 		}
-		for (int i=0; i<roleGroups.size(); i++) {
-			RoleGroup rg = (RoleGroup) roleGroups.elementAt(i);
-			List<RolePair> pairs = rg.getRolePairs();
-			for (int k=0; k<pairs.size(); k++) {
-				RolePair rp = (RolePair) pairs.get(k);
-				Restriction r1 = rp.getRole1();
-				Restriction r2 = rp.getRole1();
-				String label = (String) r1.getRoleLabel();
-				String range = (String) roleName2RangeNameMap.get(label);
-				if (!ranges.contains(range)) {
-					ranges.add(range);
-				}
-				label = (String) r2.getRoleLabel();
-				range = (String) roleName2RangeNameMap.get(label);
-				if (!ranges.contains(range)) {
-					ranges.add(range);
+		if (roleGroups != null && roleGroups.size() > 0) {
+			for (int i=0; i<roleGroups.size(); i++) {
+				RoleGroup rg = (RoleGroup) roleGroups.elementAt(i);
+				List<RolePair> pairs = rg.getRolePairs();
+				for (int k=0; k<pairs.size(); k++) {
+					RolePair rp = (RolePair) pairs.get(k);
+					Restriction r1 = rp.getRole1();
+					Restriction r2 = rp.getRole1();
+					String label = (String) r1.getRoleLabel();
+					String range = (String) roleName2RangeNameMap.get(label);
+					if (!ranges.contains(range)) {
+						ranges.add(range);
+					}
+					label = (String) r2.getRoleLabel();
+					range = (String) roleName2RangeNameMap.get(label);
+					if (!ranges.contains(range)) {
+						ranges.add(range);
+					}
 				}
 			}
 		}
@@ -259,59 +267,63 @@ public class LogicalExpressionFormatter {
 	public boolean valiateRangesInLEData(HashMap hmap) {
 		Vector roleUnions = (Vector) hmap.get("ROLE UNION");
 		Vector roleGroups = (Vector) hmap.get("ROLE GROUP");
-		for (int i=0; i<roleUnions.size(); i++) {
-			RoleUnion ru = (RoleUnion) roleUnions.elementAt(i);
-			List<Restriction> restrictions = ru.getRoles();
-			Restriction r = (Restriction) restrictions.get(0);
-			String label = (String) r.getRoleLabel();
-			String range = (String) roleName2RangeNameMap.get(label);
-			boolean bool = true;
-			for (int j=1; j<restrictions.size(); j++) {
-				r = (Restriction) restrictions.get(j);
-				label = (String) r.getRoleLabel();
-				String range_j = (String) roleName2RangeNameMap.get(label);
-				if (range_j.compareTo(range) != 0) {
-					bool = false;
-					break;
+		if (roleUnions != null && roleUnions.size() > 0) {
+			for (int i=0; i<roleUnions.size(); i++) {
+				RoleUnion ru = (RoleUnion) roleUnions.elementAt(i);
+				List<Restriction> restrictions = ru.getRoles();
+				Restriction r = (Restriction) restrictions.get(0);
+				String label = (String) r.getRoleLabel();
+				String range = (String) roleName2RangeNameMap.get(label);
+				boolean bool = true;
+				for (int j=1; j<restrictions.size(); j++) {
+					r = (Restriction) restrictions.get(j);
+					label = (String) r.getRoleLabel();
+					String range_j = (String) roleName2RangeNameMap.get(label);
+					if (range_j.compareTo(range) != 0) {
+						bool = false;
+						break;
+					}
 				}
-			}
-			if (!bool) {
-				System.out.println("Invalid RoleUnion: ");
-				System.out.println(ru.toJson());
+				if (!bool) {
+					System.out.println("Invalid RoleUnion: ");
+					System.out.println(ru.toJson());
+				}
 			}
 		}
-		for (int i=0; i<roleGroups.size(); i++) {
-			RoleGroup rg = (RoleGroup) roleGroups.elementAt(i);
-			List<RolePair> pairs = rg.getRolePairs();
-			RolePair rp = (RolePair) pairs.get(0);
-			Restriction r1 = rp.getRole1();
-			String label = (String) r1.getRoleLabel();
-			String range = (String) roleName2RangeNameMap.get(label);
-			Restriction r2 = rp.getRole2();
-			label = (String) r1.getRoleLabel();
-			String range_2 = (String) roleName2RangeNameMap.get(label);
-			if (range_2.compareTo(range) != 0) {
-				System.out.println("Invalid RoleGroup: ");
-				System.out.println(rg.toJson());
-				return false;
-			}
-		    for (int k=1; k<pairs.size(); k++) {
-				rp = (RolePair) pairs.get(k);
-				r1 = rp.getRole1();
-				r2 = rp.getRole1();
+		if (roleGroups != null && roleGroups.size() > 0) {
+			for (int i=0; i<roleGroups.size(); i++) {
+				RoleGroup rg = (RoleGroup) roleGroups.elementAt(i);
+				List<RolePair> pairs = rg.getRolePairs();
+				RolePair rp = (RolePair) pairs.get(0);
+				Restriction r1 = rp.getRole1();
+				String label = (String) r1.getRoleLabel();
+				String range = (String) roleName2RangeNameMap.get(label);
+				Restriction r2 = rp.getRole2();
 				label = (String) r1.getRoleLabel();
-				range_2 = (String) roleName2RangeNameMap.get(label);
+				String range_2 = (String) roleName2RangeNameMap.get(label);
 				if (range_2.compareTo(range) != 0) {
 					System.out.println("Invalid RoleGroup: ");
 					System.out.println(rg.toJson());
 					return false;
 				}
-				label = (String) r2.getRoleLabel();
-				range_2 = (String) roleName2RangeNameMap.get(label);
-				if (range_2.compareTo(range) != 0) {
-					System.out.println("Invalid RoleGroup: ");
-					System.out.println(rg.toJson());
-					return false;
+				for (int k=1; k<pairs.size(); k++) {
+					rp = (RolePair) pairs.get(k);
+					r1 = rp.getRole1();
+					r2 = rp.getRole1();
+					label = (String) r1.getRoleLabel();
+					range_2 = (String) roleName2RangeNameMap.get(label);
+					if (range_2.compareTo(range) != 0) {
+						System.out.println("Invalid RoleGroup: ");
+						System.out.println(rg.toJson());
+						return false;
+					}
+					label = (String) r2.getRoleLabel();
+					range_2 = (String) roleName2RangeNameMap.get(label);
+					if (range_2.compareTo(range) != 0) {
+						System.out.println("Invalid RoleGroup: ");
+						System.out.println(rg.toJson());
+						return false;
+					}
 				}
 			}
 		}
@@ -332,36 +344,41 @@ public class LogicalExpressionFormatter {
 		Vector roleUnions = (Vector) hmap.get("ROLE UNION");
 		List<RoleUnion> roleUnion_list = new ArrayList();
 		List roleList = new ArrayList();
-		for (int i=0; i<roleUnions.size(); i++) {
-			RoleUnion ru = (RoleUnion) roleUnions.elementAt(i);
-			List<Restriction> restrictions = ru.getRoles();
-			for (int j=0; j<restrictions.size(); j++) {
-				Restriction r = (Restriction) restrictions.get(0);
-				String label = (String) r.getRoleLabel();
-				String range_2 = (String) roleName2RangeNameMap.get(label);
-				if (range_2.compareTo(range) == 0) {
-					roleList.add(r);
+		if (roleUnions != null && roleUnions.size() > 0) {
+			for (int i=0; i<roleUnions.size(); i++) {
+				RoleUnion ru = (RoleUnion) roleUnions.elementAt(i);
+				List<Restriction> restrictions = ru.getRoles();
+				for (int j=0; j<restrictions.size(); j++) {
+					Restriction r = (Restriction) restrictions.get(0);
+					String label = (String) r.getRoleLabel();
+					String range_2 = (String) roleName2RangeNameMap.get(label);
+					if (range_2.compareTo(range) == 0) {
+						roleList.add(r);
+					}
 				}
-			}
-			if (roleList.size() > 1) {
-				RoleUnion ru_i = new RoleUnion(roleList);
-				roleUnion_list.add(ru_i);
-			} else if (roleList.size() == 1) {
-				simpleRoleList.add((Restriction) roleList.get(0));
+				if (roleList.size() > 1) {
+					RoleUnion ru_i = new RoleUnion(roleList);
+					roleUnion_list.add(ru_i);
+				} else if (roleList.size() == 1) {
+					simpleRoleList.add((Restriction) roleList.get(0));
+				}
 			}
 		}
 
 		Vector roleGroups = (Vector) hmap.get("ROLE GROUP");
 		List<RoleGroup> roleGroup_list = new ArrayList();
-		for (int i=0; i<roleGroups.size(); i++) {
-			RoleGroup rg = (RoleGroup) roleGroups.elementAt(i);
-			List<RolePair> pairs = rg.getRolePairs();
-			RolePair rp = (RolePair) pairs.get(0);
-			Restriction r1 = rp.getRole1();
-			String label = (String) r1.getRoleLabel();
-			String range_3 = (String) roleName2RangeNameMap.get(label);
-			if (range_3.compareTo(range) == 0) {
-				roleGroup_list.add(rg);
+		if (roleGroups != null && roleGroups.size() > 0) {
+			//List<RoleGroup> roleGroup_list = new ArrayList();
+			for (int i=0; i<roleGroups.size(); i++) {
+				RoleGroup rg = (RoleGroup) roleGroups.elementAt(i);
+				List<RolePair> pairs = rg.getRolePairs();
+				RolePair rp = (RolePair) pairs.get(0);
+				Restriction r1 = rp.getRole1();
+				String label = (String) r1.getRoleLabel();
+				String range_3 = (String) roleName2RangeNameMap.get(label);
+				if (range_3.compareTo(range) == 0) {
+					roleGroup_list.add(rg);
+				}
 			}
 		}
 
@@ -512,6 +529,12 @@ public class LogicalExpressionFormatter {
 		gov.nih.nci.evs.restapi.appl.LogicalExpression le = new gov.nih.nci.evs.restapi.appl.LogicalExpression(serviceUrl, named_graph, username, password);
 		String expression = run(le, named_graph, code);
 		System.out.println(expression);
+		/*
+        LogicalExpressionFormatter formatter = new LogicalExpressionFormatter(le.getRoleCode2RangeNameMap(), le.getRoleName2RangeNameMap());
+		String code = args[0];
+        String expression = formatter.getLogialExpression(le, named_graph, code);
+        System.out.println(expression);
+        */
 	}
 }
 
