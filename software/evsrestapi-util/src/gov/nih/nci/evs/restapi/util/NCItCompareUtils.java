6f8752c7e030e6dd4ab281f9a8791671a5ae0f9d
kimongl <kim.ong@nih.gov>
2026-06-26 05:06:04 -0400

    Updated NCItCompareUtils.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/NCItCompareUtils.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/NCItCompareUtils.java
@@ -7,6 +7,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import java.util.Comparator;
+import java.util.stream.Stream;
+import java.util.stream.*;
+
 public class NCItCompareUtils {
 	String observedDir = null;
 	String expectedDir = null;
@@ -27,6 +31,25 @@ public class NCItCompareUtils {
 		return v.size();
     }
 
+    public static List listFilesInDirectory(String dir) {
+		List<String> result = null;
+		try {
+			String currentPath = new java.io.File(".").getCanonicalPath();
+			Path startPath = Paths.get(currentPath + File.separator + dir);
+			try (Stream<Path> stream = Files.walk(startPath)) {
+				result = stream
+					.map(Path::toString)
+					.collect(Collectors.toList());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	} catch (Exception ex) {
+
+		}
+		return result;
+    }
+
+
     public void run() {
 		Vector w = new Vector();
 		String path = FileUtils.getCurrentWorkingDirectory() + File.separator + observedDir;
@@ -43,6 +66,56 @@ public class NCItCompareUtils {
 			}
 		}
 		Utils.saveToFile(observedDir + "_vs_" + expectedDir + ".txt", w);
+	}
+
+    // In v2 but not in v1
+	public static Vector v1_v2(Vector v1, Vector v2) {
+		Vector w = new Vector();
+        HashSet hset = Utils.vector2HashSet(v2);
+        for (int i=0; i<v1.size(); i++) {
+			String t = (String) v1.elementAt(i);
+			if (!hset.contains(t)) {
+				w.add(t);
+			}
+		}
+		return new SortUtils().quickSort(w);
+	}
+
+	public void runDiff() {
+		String dir1 = observedDir;
+		String dir2 = expectedDir;
+		runDiff(dir1, dir2);
+	}
+
+	public static void runDiff(String dir1, String dir2) {
+		List list1 = listFilesInDirectory(dir1);
+		int lcv = 0;
+		Vector v1_not_in_v2 = new Vector();
+		Vector v2_not_in_v1 = new Vector();
+		for (int i=0; i<list1.size(); i++) {
+			String t1 = (String) list1.get(i);
+			File f1 = new File(t1);
+			if (!f1.isDirectory()) {
+				String t2 = t1.replace(dir1, dir2);
+				File f2 = new File(t2);
+				if (!f1.exists()) {
+					System.out.println("File " + f1.getAbsolutePath() + " does not exist.");
+				} else if (!f2.exists()) {
+					System.out.println("File " + f2.getAbsolutePath() + " does not exist.");
+				} else {
+					lcv++;
+					Vector v1 = Utils.readFile(f1.getAbsolutePath());
+					Vector v2 = Utils.readFile(f2.getAbsolutePath());
+					v1_not_in_v2.add("\n(" + lcv + ") Lines in " + t1 + " but not in " + t2);
+					v1_not_in_v2.addAll(v1_v2(v1, v2));
+
+					v2_not_in_v1.add("\n(" + lcv + ") Lines in " + t2 + " but not in " + t1);
+					v2_not_in_v1.addAll(v1_v2(v2, v1));
+				}
+			}
+		}
+		Utils.saveToFile(dir1 + "_not_in_" + dir2 + ".txt", v1_not_in_v2);
+		Utils.saveToFile(dir2 + "_not_in_" + dir1 + ".txt", v2_not_in_v1);
 	}
 
 	public static void main(String[] args) {
