22143e6f009c06dd986dd28ae445d1c80a68fbd6
kimongl <kim.ong@nih.gov>
2026-07-18 19:58:37 -0400

    Updated SpecialCharHandler.java.

--- software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/SpecialCharHandler.java
+++ software/evsrestapi-util/src/gov/nih/nci/evs/restapi/util/SpecialCharHandler.java
@@ -1,96 +1,119 @@
 package gov.nih.nci.evs.restapi.util;
 import java.io.*;
 import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 //Reference: https://en.wikipedia.org/wiki/List_of_typographical_symbols_and_punctuation_marks
+//https://unicodeplus.com/U+2014
 
 public class SpecialCharHandler {
-	static HashMap replacementHashMap = null;
-	static String ONTOLOGY_INFO_FILE = "ontology_info.owl";
-    String owlfile = null;
-	OWLClassLoader loader = null;
-	HashMap classDataHashMap = null;
-	Vector classIdVec = null;
 
-	static {
-		replacementHashMap = new HashMap();
-        replacementHashMap.put("â€™", "—"); //Emdash
-	}
-
-    public SpecialCharHandler(String owlfile) {
-		this.owlfile = owlfile;
-		initialize();
-	}
-
-	public void initialize() {
-		loader = new OWLClassLoader(owlfile);
-		classDataHashMap = loader.getClassDataHashMap();
-		classIdVec = loader.getClassIdVec();
-		extractOntologyInfo(Utils.readFile(owlfile));
-	}
-
-    public void extractOntologyInfo(Vector v) {
-	    int istart = 0;
-	    String target = "</owl:Ontology>";
-	    int iend = TextFileExtractor.findLineNumber(v, target) + 3;
-	    String outputfile = ONTOLOGY_INFO_FILE;
-	    Vector w = TextFileExtractor.extractLines(v, istart, iend);
-	    Utils.saveToFile(ONTOLOGY_INFO_FILE, w);
-	}
-
-	public String generateSampleOWL(String code) {
-		Vector w = new Vector();
-		w.addAll(Utils.readFile(ONTOLOGY_INFO_FILE));
-		w.addAll((Vector) classDataHashMap.get(code));
-		w.add("</rdf:RDF>");
-		String outputfile = code + ".owl";
-		Utils.saveToFile(outputfile, w);
-		return outputfile;
-	}
-
-
-	public static String run(String line) {
-		Iterator it = replacementHashMap.keySet().iterator();
-		while (it.hasNext()) {
-			String replace = (String) it.next();
-			String by = (String) replacementHashMap.get(replace);
-			line = line.replace(replace, by);
-		}
-		return line;
-	}
-
-	public static Vector run(Vector v) {
+	public static Vector search(Vector v, String target) {
 		Vector w = new Vector();
 		for (int i=0; i<v.size(); i++) {
+			int j = i+1;
 			String line = (String) v.elementAt(i);
-			line = run(line);
-			w.add(line);
+			if (line.indexOf(target) != -1) {
+				w.add("LINE " + j + ": " + line);
+			}
 		}
 		return w;
 	}
 
-	public static Vector substituteSpecChars(String filename) {
-		return run(Utils.readFile(filename));
+	public static String backupFile(String filename) {
+		String outputfile = "copy_of_" + filename;
+		FileUtils.copyfile(filename, outputfile);
+		return outputfile;
 	}
 
-	public static void main(String[] args) {
-		long ms = System.currentTimeMillis();
-		//////////////////////////////////////////////////////////////////
-		String owlfile = args[0];
-		String code = args[1];
-		System.out.println("NCIt owlfile: " + owlfile);
-		System.out.println("code: " + code);
-		SpecialCharHandler test = new SpecialCharHandler(owlfile);
-
-		System.out.println("generating class owl: " + code + ".owl");
-		String outputfile = test.generateSampleOWL(code);
-		System.out.println("generated class owl: " + outputfile);
-		//////////////////////////////////////////////////////////////////
-
-		System.out.println("substituteSpecChars ... ");
-		Vector w = substituteSpecChars(outputfile);
-		Utils.saveToFile(outputfile, w);
-        System.out.println("\tTotal run time (ms): " + (System.currentTimeMillis() - ms));
+	public static String copyFile(String filename) {
+		String outputfile = "copy_of_" + filename;
+		Vector v = Utils.readFile(filename);
+		Utils.saveToFile(outputfile, v);
+		return outputfile;
 	}
 
+    public static int getByteLength(String text) {
+ 	    try {
+	    	byte[] bytesInUTF8 = text.getBytes("UTF-8");
+            return bytesInUTF8.length;
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return -1;
+	}
+
+    public static void displayBytes(String text) {
+ 	    try {
+	    	byte[] bytesInUTF8 = text.getBytes("UTF-8");
+	    	for (int i=0; i<bytesInUTF8.length; i++) {
+				byte b = bytesInUTF8[i];
+				System.out.println(b);
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+    public static Vector substituteSpecialCharInFile(String filename, String replace, String by) {
+		Vector lines = Utils.readFile(filename);
+		return substituteSpecialChar(lines, replace, by);
+	}
+
+    public static Vector substituteSpecialChar(Vector lines, String replace, String by) {
+	    Vector w = new Vector();
+	    for (int i=0; i<lines.size(); i++) {
+		    String line = (String) lines.elementAt(i);
+		    w.add(substituteSpecialChar(line, replace, by));
+	    }
+	    return w;
+    }
+
+    public static String substituteSpecialChar(String text, String replace, String by) {
+		int n = text.indexOf(replace);
+		if (n == -1) return text;
+		int num_text_bytes = getByteLength(text);
+		int num_replace_bytes = getByteLength(replace);
+		int num_by_bytes = getByteLength(by);
+		int len = num_text_bytes - num_replace_bytes + num_by_bytes;
+		byte[] byteArray = new byte[len];
+        String s = null;
+        try {
+			byte[] bytesInUTF8_text = text.getBytes("UTF-8");
+			byte[] bytesInUTF8_replace = replace.getBytes("UTF-8");
+			byte[] bytesInUTF8_by = by.getBytes("UTF-8");
+
+            int m = 0;
+			for (int i=0; i<n; i++) {
+				byteArray[i] = bytesInUTF8_text[i];
+				m++;
+			}
+			int j = 0;
+			for (int i=n; i<n+num_by_bytes; i++) {
+   			    byteArray[i] = bytesInUTF8_by[j];
+   			    j++;
+   			    m++;
+			}
+			j = num_by_bytes;
+			for (int i=m; i<len; i++) {
+				byteArray[i] = bytesInUTF8_text[num_text_bytes - num_replace_bytes + j];
+				j++;
+			}
+			s = new String(byteArray, StandardCharsets.UTF_8);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		Vector w = new Vector();
+		w.add(text);
+		w.add(replace);
+		w.add(by);
+		w.add(s);
+		Utils.saveToFile("test.txt", w);
+
+		return s;
+	}
 }
